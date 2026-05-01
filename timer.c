00e0f0b8dedf3d64cfa38e241b1ba9c18ba29790
gabrielrouse <grouse@calpoly.edu>
2026-04-30 18:37:44 -0700

    Update timer.c

--- timer.c
+++ timer.c
@@ -1,36 +1,43 @@
 /*
  *******************************************************************************
  * @file           : timer.c
- * @brief          : X
- * project         : EE 329 S'26 AX
- * authors         : joeym
- * version         : 0.1
- * date            : Apr 27, 2026
+ * @brief          : TIM2 and MCO support for reaction timer operation
+ * project         : EE 329 S'26 A4
+ * authors         : Joseph Matella and Gabriel Rouse
+ * version         : 1.0
+ * date            : 20260430
  * compiler        : STM32CubeIDE v.1.19.0 Build: 14980_20230301_1550 (UTC)
  * target          : NUCLEO-L4A6ZG
  * clocks          : 4 MHz MSI to AHB2
- * @attention      : (c) 2026 STMicroelectronics.  All rights reserved.
+ * @attention      : (c) 2026 STMicroelectronics. All rights reserved.
  *******************************************************************************
- * Description: X
+ * Description:
+ * Configures TIM2 as a free-running reaction timer and timeout source for the
+ * final reaction timer game. Also provides MCO clock output setup for external
+ * oscilloscope measurement of the 4 MHz system clock.
  *
  *******************************************************************************
  * GPIO Wiring
  * |   Component    | GPIO Identifier | Connector Location | Config
  *-----------------------------------------------------------------------------
- * | LCD - DB4 - 11 | PC0             | CN9-3              | OUT
+ * | MCO Clock Out  | PA8             | CN10-23            | AF0
  *******************************************************************************
  * Version History
  *  Ver.|   Date   |  Description
  *  ---------------------------------------------------------------------------
- *      |          | 
+ *  1.0 | 20260430 | Finalized reaction timer TIM2 control module
  *******************************************************************************
- *
- * Header format adapted from [Code Appendix by Kevin Vo] pg 5
  */
-
 #include "timer.h"
 #include "main.h"
 
+/* -----------------------------------------------------------------------------
+ * function : setup_TIM2()
+ * INs      : none
+ * OUTs     : none
+ * action   : configures TIM2 as a free-running timer with CCR1 set for a
+ *            10-second timeout and enables the TIM2 interrupt in the NVIC
+ * -------------------------------------------------------------------------- */
 void setup_TIM2(void) {
    RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN;           // enable clock for TIM2
    TIM2->ARR = 0xFFFFFFFF;                         // Set ARR to max
@@ -40,6 +47,13 @@ void setup_TIM2(void) {
    __enable_irq();                                 // global IRQ enable
 }
 
+/* -----------------------------------------------------------------------------
+ * function : start_TIM2()
+ * INs      : none
+ * OUTs     : none
+ * action   : resets TIM2 count, clears pending flags, enables interrupts, and
+ *            starts the timer for a new reaction timing interval
+ * -------------------------------------------------------------------------- */
 void start_TIM2(void) {
    TIM2->DIER |= (TIM_DIER_CC1IE | TIM_DIER_UIE);  // enable event gen, rcv CCR1
    TIM2->CNT = 0;                                  // Reset Count
@@ -47,15 +61,34 @@ void start_TIM2(void) {
    TIM2->CR1 |= TIM_CR1_CEN;                       // start TIM2 CR1
 }
 
+/* -----------------------------------------------------------------------------
+ * function : stop_TIM2()
+ * INs      : none
+ * OUTs     : none
+ * action   : stops TIM2 and disables its compare and update interrupts
+ * -------------------------------------------------------------------------- */
 void stop_TIM2(void) {
    TIM2->CR1 &= ~TIM_CR1_CEN;                       // STOP TIM2 CR1
    TIM2->DIER &= ~(TIM_DIER_CC1IE | TIM_DIER_UIE);  // disable event gen, rcv CCR1
 }
 
+/* -----------------------------------------------------------------------------
+ * function : readCount_TIM2()
+ * INs      : none
+ * OUTs     : returns current TIM2 count value
+ * action   : reads the current counter value from TIM2 for reaction timing
+ * -------------------------------------------------------------------------- */
 uint32_t readCount_TIM2(void) {
 	return TIM2->CNT;
 }
 
+/* -----------------------------------------------------------------------------
+ * function : setup_MCO_CLK()
+ * INs      : none
+ * OUTs     : none
+ * action   : configures PA8 as MCO output and routes the 4 MHz system clock
+ *            to that pin for oscilloscope measurement
+ * -------------------------------------------------------------------------- */
 void setup_MCO_CLK(void) {
    // enable MCO, MCOSEL = 0b0001 to select SYSCLK = MSI (4 MHz source)
    RCC->CFGR = ((RCC->CFGR & ~(RCC_CFGR_MCOSEL)) | (RCC_CFGR_MCOSEL_0));
@@ -69,7 +102,13 @@ void setup_MCO_CLK(void) {
    GPIOA->AFR[1]  &= ~(GPIO_AFRH_AFSEL8);    	// select MCO (AF0) on PA8 (AFRH)
 }
 
-// Note: if square wave not output, press reset button on nucleo board
+/* -----------------------------------------------------------------------------
+ * function : TIM2_IRQHandler()
+ * INs      : none
+ * OUTs     : none
+ * action   : handles TIM2 compare and update interrupts; on CCR1 timeout the
+ *            reaction timer is stopped and the program returns to reset state
+ * -------------------------------------------------------------------------- */
 void TIM2_IRQHandler(void) {
    if (TIM2->SR & TIM_SR_CC1IF) {      // triggered by CCR1 event ...
       TIM2->SR &= ~(TIM_SR_CC1IF);     // manage the flag
@@ -80,6 +119,3 @@ void TIM2_IRQHandler(void) {
       TIM2->SR &= ~(TIM_SR_UIF);       // manage the flag
    }
 }
-
-
-
