76a3aa807ffc1cc6285848181d4311b9f9267003
gabrielrouse <grouse@calpoly.edu>
2026-04-30 19:23:29 -0700

    Update delay.h

--- delay.h
+++ delay.h
@@ -1,20 +1,24 @@
 /*
  *******************************************************************************
  * @file           : delay.h
- * @brief          : header file for delay.h
- * masks and defines
- * project         : EE 329 S'26 A3
+ * @brief          : header file for delay.c
+ * project         : EE 329 S'26 A4
  * authors         : Joseph Matella and Gabriel Rouse
- * version         : 0.1
- * date            : 20260421
+ * version         : 1.0
+ * date            : 20260430
  * compiler        : STM32CubeIDE v.1.19.0 Build: 14980_20230301_1550 (UTC)
  * target          : NUCLEO-L4A6ZG
  * clocks          : 4 MHz MSI to AHB2
- * @attention      : (c) 2026 STMicroelectronics.  All rights reserved.
+ * @attention      : (c) 2026 STMicroelectronics. All rights reserved.
+ *******************************************************************************
+ * Description:
+ * Declares SysTick-based microsecond delay support functions used throughout
+ * the reaction timer application.
+ *
  *******************************************************************************
  * Header format adapted from [Code Appendix by Kevin Vo] pg 5
+ *******************************************************************************
  */
-
 #ifndef INC_DELAY_H_
 #define INC_DELAY_H_
 
